33dea64fc7b8f8cb0b787d643ac2683afb6b9c7f
Jeonghoon Park <hoonably@gmail.com>
2025-04-28 22:36:53 +0900

    setting functions

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -6,6 +6,23 @@
 
 static void syscall_handler (struct intr_frame *);
 
+// ✅✅✅✅✅
+typedef int pid_t;
+static void syscall_handler (struct intr_frame *);
+void halt(void);              // 시스템 종료
+void exit(int status);        // 프로그램 종료
+pid_t exec(const char *cmd_line); // 새 프로그램 실행
+int wait(pid_t pid);          // 자식 프로세스 기다리기
+bool create(const char *file, unsigned initial_size); // 파일 만들기
+bool remove(const char *file); // 파일 삭제하기
+int open(const char *file);    // 파일 열기
+int filesize(int fd);          // 파일 크기 알아오기
+int read(int fd, void *buffer, unsigned size); // 파일이나 키보드에서 읽기
+int write(int fd, const void *buffer, unsigned size); // 파일이나 콘솔에 쓰기
+void seek(int fd, unsigned position); // 파일 읽기/쓰기 위치 바꾸기
+unsigned tell(int fd);         // 파일 현재 위치 알아오기
+void close(int fd);            // 파일 닫기
+
 void
 syscall_init (void) 
 {
