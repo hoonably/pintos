3863b533f2da852aeba8ddc73db682792de81fd4
Jeonghoon Park <hoonably@gmail.com>
2025-05-19 01:01:24 +0900

    bad passed

--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -3,6 +3,7 @@
 
 void syscall_init (void);
 
-// ✅✅✅✅✅ - 모범답안대로라면 여기 한줄이 추가되야하는데 뭘까
+// ✅✅✅✅✅ - 모범답안대로라면 여기 한줄이 추가 -> exception.c에서 exit() 호출하기 위해서 정의하는듯
+void exit(int status);
 
 #endif /* userprog/syscall.h */
